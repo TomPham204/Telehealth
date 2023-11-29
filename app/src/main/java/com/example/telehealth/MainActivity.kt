7c35669c4d2be4faed06a67c237aca2bc8e96ed5
Tom Pham <46714051+TomPham204@users.noreply.github.com>
2023-11-29 21:22:00 +0700

    update profile screen
    login/logout using admin@gmail.com | admin working. local mock db not implemented

--- app/src/main/java/com/example/telehealth/MainActivity.kt
+++ app/src/main/java/com/example/telehealth/MainActivity.kt
@@ -2,10 +2,42 @@ package com.example.telehealth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.telehealth.profile.ProfileFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bottomNavigationView: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation)
+
+        // Set default fragment (ProfileFragment) when the app starts
+        if (savedInstanceState == null) {
+            val profileFragment = ProfileFragment()
+            supportFragmentManager.beginTransaction().replace(R.id.fragment_container, profileFragment)
+                .commit()
+        }
+
+        // Handle item clicks in the bottom navigation
+        bottomNavigationView.setOnNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.menu_profile -> {
+                    replaceFragment(ProfileFragment())
+                    true
+                }
+                // Add logic for other menu items if needed
+                else -> false
+            }
+        }
+    }
+
+    fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().replace(R.id.fragment_container, fragment)
+            .addToBackStack(null)
+            .commit()
     }
 }
--- app/src/main/java/com/example/telehealth/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/telehealth/profile/ProfileFragment.kt
@@ -1,41 +1,114 @@
 package com.example.telehealth.profile
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.viewbinding.ViewBinding
+import com.example.telehealth.MainActivity
 import com.example.telehealth.R
 import com.example.telehealth.databinding.ProfileFragmentBinding
+import com.example.telehealth.databinding.LoginScreenBinding
 
-class ProfileFragment: Fragment() {
+class ProfileFragment : Fragment() {
     private lateinit var viewModel: ProfileViewModel
+    private var isLoggedIn = false
+    private lateinit var binding: ViewBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding: ProfileFragmentBinding = DataBindingUtil.inflate(
-            inflater, R.layout.profile_fragment, container, false
-        )
+        isLoggedIn=checkLoginStatus()
+        return if (isLoggedIn) {
+            val profileBinding: ProfileFragmentBinding = DataBindingUtil.inflate(
+                inflater, R.layout.profile_fragment, container, false
+            )
+            displayProfileInfo(profileBinding)
 
-        // Initialize ViewModel
-        viewModel = ViewModelProvider(this).get(ProfileViewModel::class.java)
+            profileBinding.logoutButton.setOnClickListener {
+                isLoggedIn=false
+                saveLoginStatus(isLoggedIn)
+                remountProfileFragment()
+            }
 
-        // Observe profile data from ViewModel
-        viewModel.userProfile.observe(viewLifecycleOwner) { userProfile ->
-            // Update UI with fetched profile data
-            binding.textViewName.text = "Name: ${userProfile.userName}"
-            binding.textViewEmail.text = "Email: ${userProfile.userEmail}"
-            binding.textViewPhone.text = "Phone: ${userProfile.userPhoneNumber}"
+            profileBinding.root
+        } else {
+            binding = LoginScreenBinding.inflate(inflater, container, false)
+            showLoginForm()
+            binding.root
         }
+    }
 
-        // Fetch profile data (call this from ViewModel)
-        viewModel.fetchUserProfile()
+    private fun checkLoginStatus(): Boolean {
+        return try {
+            val sharedPreferences = requireActivity().getPreferences(Context.MODE_PRIVATE)
+            sharedPreferences.getBoolean("IS_LOGGED_IN", false)
+        } catch(error: Exception) {
+            Log.d("checkLoginStatus", error.toString())
+            false
+        }
+    }
 
-        return binding.root
+    private fun saveLoginStatus(isLoggedIn: Boolean) {
+        val sharedPreferences = requireActivity().getPreferences(Context.MODE_PRIVATE)
+        with(sharedPreferences.edit()) {
+            putBoolean("IS_LOGGED_IN", isLoggedIn)
+            apply()
+        }
+    }
+
+    private fun showLoginForm() {
+        val loginBinding = binding as? LoginScreenBinding
+
+        loginBinding?.submitButton?.setOnClickListener {
+            val email = loginBinding.emailEditText.text.toString()
+            val password = loginBinding.passwordEditText.text.toString()
+
+            if (email == "admin@gmail.com" && password == "admin") {
+                isLoggedIn = true
+                saveLoginStatus(isLoggedIn)
+                Log.d("showLoginForm","Login details correct")
+                remountProfileFragment()
+            } else {
+                Toast.makeText(requireContext(), "Unrecognized account", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun remountProfileFragment() {
+        try {
+            val mainActivity = activity as? MainActivity
+            mainActivity?.replaceFragment(ProfileFragment())
+        }
+        catch(error: Exception) {
+            Log.d("remountProfileFragment", error.toString())
+        }
+    }
+
+    private fun displayProfileInfo(binding: ProfileFragmentBinding) {
+        // Display current profile information
+        // Fetch and display user profile data or a default profile
+        val userProfile = getUserProfile()
+        binding.textViewName.text = "Name: ${userProfile.userName}"
+        binding.textViewEmail.text = "Email: ${userProfile.userEmail}"
+        binding.textViewPhone.text = "Phone: ${userProfile.userPhoneNumber}"
+    }
+
+    private fun getUserProfile(): UserProfile {
+        // Replace this with actual profile retrieval logic
+        return UserProfile(
+            userId = "0",
+            userName = "Tuan",
+            userEmail = "tuan@example.com",
+            userPhoneNumber = "+1234567890"
+        )
     }
 }
