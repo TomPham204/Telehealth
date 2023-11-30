c3ecadddf9d2d09c294a0961245674e53bf455b8
Tom Pham <46714051+TomPham204@users.noreply.github.com>
2023-11-30 09:10:10 +0700

    return to MainActivity after ending video call

--- app/src/main/java/com/example/telehealth/VideoActivity.kt
+++ app/src/main/java/com/example/telehealth/VideoActivity.kt
@@ -3,6 +3,7 @@ package com.example.telehealth
 import android.os.Bundle
 import android.widget.FrameLayout
 import androidx.appcompat.app.AppCompatActivity
+import io.agora.agorauikit_android.AgoraButton
 import io.agora.agorauikit_android.AgoraConnectionData
 import io.agora.agorauikit_android.AgoraSettings
 import io.agora.agorauikit_android.AgoraVideoViewer
@@ -20,14 +21,29 @@ class VideoActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_video)
 
+        val exitButton = AgoraButton(this)
+        exitButton.setImageResource(R.drawable.exit_icon)
+        exitButton.setOnClickListener{
+            finish()
+        }
+
+        val agoraSettings = AgoraSettings()
+        agoraSettings.enabledButtons = mutableSetOf(AgoraSettings.BuiltinButton.CAMERA, AgoraSettings.BuiltinButton.MIC,AgoraSettings.BuiltinButton.FLIP)
+        agoraSettings.extraButtons= mutableListOf(exitButton)
+
         agView = AgoraVideoViewer(
             this,
             AgoraConnectionData(appId, token),
-            AgoraVideoViewer.Style.FLOATING,
-            AgoraSettings(),
+            AgoraVideoViewer.Style.GRID,
+            agoraSettings,
             null
         )
         this.addContentView(agView, FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT))
         agView!!.join(channel, role=Constants.CLIENT_ROLE_BROADCASTER)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        finish()
     }
 }
