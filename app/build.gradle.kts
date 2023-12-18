62f3a5e02e1fb1b101ae9ef08e3ae8f55fa7136a
Tom Pham <46714051+TomPham204@users.noreply.github.com>
2023-12-18 22:22:07 +0700

    add graddle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,8 @@ dependencies {
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.databinding:databinding-runtime:8.2.0")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.3.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
